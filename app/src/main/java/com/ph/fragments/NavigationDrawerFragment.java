5dcd69a288ea790713ca29d70d24a4d4977849a2
Anand Sharma <saurabh713@gmail.com>
2016-01-12 18:18:11 +0530

    Recycler view for Navigation drawer

--- app/src/main/java/com/ph/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/ph/fragments/NavigationDrawerFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +16,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ph.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Anand on 12/26/2016.
@@ -24,10 +29,13 @@ public class NavigationDrawerFragment extends Fragment {
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+    private RecyclerView mRecyclerView;
 
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
     private View containerView;
+    private DrawerAdapter adapter;
+
     public NavigationDrawerFragment() {
 
     }
@@ -41,10 +49,26 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        View drawerLayout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        mRecyclerView = (RecyclerView) drawerLayout.findViewById(R.id.drawerList);
+        adapter = new DrawerAdapter(getActivity(),getData());
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return drawerLayout;
     }
 
+    public List<DrawerListData> getData() {
+        List<DrawerListData> data = new ArrayList<>();
+        int[] images = {R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher};
+        String[] titles = {"Messages", "Titles", "Progress", "Reward"};
+        for (int i = 0; i < images.length; i++) {
+            DrawerListData itemData = new DrawerListData();
+            itemData.setTitle(titles[i]);
+            itemData.setListId(images[i]);
+            data.add(itemData);
+        }
+        return data;
+    }
     public void setUp(DrawerLayout drawerlayout, Toolbar toolbar) {
         mDrawerLayout = drawerlayout;
         mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerlayout, toolbar, R.string.drawer_open,R.string.drawer_close){
