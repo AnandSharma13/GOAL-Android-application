1a69f36cea9f3f9f45e59e2e6617a41ce53398a0
Anand Sharma <saurabh713@gmail.com>
2016-01-21 08:13:02 -0500

    Nutrition entry User Interface

--- app/src/main/java/com/ph/Activities/NutritionEntryCreate.java
+++ app/src/main/java/com/ph/Activities/NutritionEntryCreate.java
@@ -4,6 +4,11 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
 
 import com.ph.R;
 import com.ph.Utils.DateOperations;
@@ -17,11 +22,18 @@ import com.ph.net.SyncUtils;
 
 import java.util.ArrayList;
 
+
 public class NutritionEntryCreate extends AppCompatActivity {
 
     DateOperations mDateOperations;
     DBOperations mDBOperations;
     SharedPreferences prefs;
+    Button mDecreaseBtn;
+    Button mIncreaseBtn;
+    TextView mOthersCountTv;
+    private Handler repeatUpdateHandler;
+    private Boolean mIncrementFlag;
+    private Boolean mDecrementFlag;
 
     public final ArrayList<String> tablesList = new ArrayList<String>() {{
         // add(User.tableName);
@@ -38,12 +50,24 @@ public class NutritionEntryCreate extends AppCompatActivity {
         mDBOperations = new DBOperations(getApplicationContext());
 
 
+        mDecreaseBtn = (Button) findViewById(R.id.nutrition_entry_create_btn_decrease);
+        mIncreaseBtn = (Button) findViewById(R.id.nutrition_entry_create_btn_increase);
+        mOthersCountTv = (TextView) findViewById(R.id.nutrition_entry_create_tv_others_count);
+        repeatUpdateHandler = new Handler();
+        mDecrementFlag = false;
+        mIncrementFlag = false;
+
+
+
+        //For testing
         addNutritionEntryToDB();
         StartEndDateObject obj = mDateOperations.getDatesForToday();
         return;
     }
 
-    public void addNutritionEntryToDB(){
+
+
+    public void addNutritionEntryToDB() {
         NutritionEntry nutritionEntry = new NutritionEntry();
         Bundle settingsBundle = new Bundle();
         SyncUtils syncUtils = new SyncUtils();
@@ -72,18 +96,30 @@ public class NutritionEntryCreate extends AppCompatActivity {
         syncUtils.TriggerRefresh(settingsBundle);
         Log.i("NutritionEntryCreate", "Sync called for nutrition entry");
 
-    }
-
-
-    public void getDayRecords(){
-
-
 
     }
 
 
+    public void getDayRecords() {
 
-        
+    }
+
+    //TODO check with for long click option
+    class RepeatUpdater implements Runnable {
+        @Override
+        public void run() {
+            if (mIncrementFlag){
+                Log.i("NutritionEntryCreate", "Increment Pressed");
+                repeatUpdateHandler.postDelayed(new RepeatUpdater(), 50);
+            }
+            else if (mDecrementFlag){
+                Log.i("NutritionEntryCreate", "Decrement Pressed");
+                repeatUpdateHandler.postDelayed(new RepeatUpdater(), 50);
+            }
+        }
+    }
+
+
 
 }
 
