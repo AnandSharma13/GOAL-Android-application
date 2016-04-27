2e4f1aa656e160eb9901ed1d472ed404ce1f1009
anupbhar <anupbhar@iu.edu>
2016-04-27 10:52:02 -0400

    Fixed the crash on clicking progress bar in progress tab.

--- app/src/main/java/com/ph/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/ph/fragments/NavigationDrawerFragment.java
@@ -73,8 +73,8 @@ public class NavigationDrawerFragment extends Fragment {
 
     public List<DrawerListData> getData() {
         List<DrawerListData> data = new ArrayList<>();
-        int[] images = {R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher};
-        String[] titles = {"Messages", "History", "Progress", "Reward","Settings","Temp"};
+        int[] images = {R.drawable.ic_launcher, R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher};
+        String[] titles = {"Messages", "History", "Progress", "Reward","Settings"};
         for (int i = 0; i < images.length; i++) {
             DrawerListData itemData = new DrawerListData();
             itemData.setTitle(titles[i]);
